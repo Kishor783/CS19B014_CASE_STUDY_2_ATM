6afa67fc2ba324166c9f69dfe8549ad2ecb6fbfe
Kishor783 <70994761+Kishor783@users.noreply.github.com>
2020-11-12 12:28:54 +0530

    Update test.java

--- ATM_CASESTUDY/test.java
+++ ATM_CASESTUDY/test.java
@@ -3,18 +3,24 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class test extends TimerTask{
-	static int an = 0;
+	int an = 0;
 	Bank b = new Bank();  // to get time limit
 	public void timerr(int accNum){ // timer method for unblocking card after "timeLimit" of time
+		
+		TimerTask tasknew = new test(accNum);
 		an = accNum;
-		TimerTask tasknew = new test();
 		Timer timer = new Timer();
 		timer.schedule(tasknew, 60000*b.getTimeLimit());
+	}
+	
+	test(int accNum){
+		this.an = accNum;
 	}
    
    public void run() {
           AccountHolder ah = new AccountHolder(an);
           ah.counter = 0;
           ah.setIsBlocked(0);
+          System.out.println(an + " pleaseeee");
    } 
 }
